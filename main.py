87ca86c4c7890cb43771c59bb7ce848e3e9ded25
Kabur <tomasfabry1@gmail.com>
2017-04-10 03:15:45 +0200

    bunch of stuff

--- main.py
+++ main.py
@@ -20,53 +20,65 @@ np.random.seed(7)
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 
-def windowAndLabel(data, windowSize):
-    X = []
-    Y = []
+def window_and_label(data, window_size):
+    x = []
+    y = []
 
-    for i in range(len(data) - windowSize):
-        X.append(data[i:(i + windowSize), 0])
-        Y.append(data[i + windowSize, 0])
-    return np.array(X), np.array(Y)
+    for i in range(len(data) - window_size):
+        x.append(data[i:(i + window_size), 0])
+        y.append(data[i + window_size, 0])
+    return np.array(x), np.array(y)
 
 
 def load_data(filename, window_size, train_ratio):
-    trainX = trainY = testX = testY = []
+    train_x = train_y = test_x = test_y = []
 
     dataframe = pandas.read_csv(filename, usecols=[2], engine='python')
     dataset = dataframe.values
     dataset = dataset.astype('float32')
-    datasetLength = len(dataset)
+    dataset_length = len(dataset)
 
     dataset = scaler.fit_transform(dataset)
 
-    # checkForZeros(dataset)
+    train_size = int(dataset_length * train_ratio)
+    testSize = len(dataset) - train_size
 
-    trainSize = int(datasetLength * train_ratio)
-    testSize = len(dataset) - trainSize
+    train_set = dataset[0:train_size]
+    test_set = dataset[train_size:len(dataset)]
 
-    trainSet = dataset[0:trainSize]
-    testSet = dataset[trainSize:len(dataset)]
+    train_x, train_y = window_and_label(train_set, timesteps)
+    test_x, test_y = window_and_label(test_set, timesteps)
 
-    trainX, trainY = windowAndLabel(trainSet, timesteps)
-    testX, testY = windowAndLabel(testSet, timesteps)
+    train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], 1))
+    test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 1))
 
-    trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-    testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
-
-    return dataset, trainX, trainY, testX, testY
+    return dataset, train_x, train_y, test_x, test_y
 
 
-def createModel(trainX, trainY, epochs, batchSize=1):
+def calculate_mape(prediction, y):
+    return np.mean(np.abs((y - prediction) / y)) * 100
+
+
+def plot_results(prediction, truth):
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111)
+    ax.plot(truth, label='True Data')
+    plt.plot(prediction, label='Prediction')
+    plt.legend()
+    plt.show()
+
+
+def createModel(train_x, train_y, epochs, batch_size):
     model = Sequential()
-    model.add(LSTM(4, batch_input_shape=(batchSize, timesteps, 1), stateful=True, return_sequences=True))
-    model.add(LSTM(4, batch_input_shape=(batchSize, timesteps, 1), stateful=True))
+    # model.add(LSTM(4, batch_input_shape=(batch_size, timesteps, 1), stateful=True, return_sequences=True))
+    model.add(LSTM(4, batch_input_shape=(batch_size, timesteps, 1), stateful=True))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     print(model.summary())
 
     for _ in range(epochs):
-        model.fit(trainX, trainY, epochs=1, batch_size=batchSize, verbose=2, shuffle=False)
+        model.fit(train_x, train_y, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
         model.reset_states()
 
     print("Finished Training!")
@@ -74,40 +86,25 @@ def createModel(trainX, trainY, epochs, batchSize=1):
     return model
 
 
-# def predictOneByOne(model, data, predictionLen):
-#     predictions = []
-#
-#     for timestep in range(int(len(data)) - predictionLen):
-#         prediction = []
-#         for j in range(predictionLen):
-#             print(data[timestep][0][0])
-#             number = model.predict(data[timestep][0][0])  # needs a 3-dimensional array here
-#             prediction.append(number)
-#
-#         predictions.append(prediction)
-#
-#     return predictions
-
-
-def predictMultiple(model, data, timesteps, predictionLength, batchSize, reset=0):
+def predictMultiple(model, data, timesteps, prediction_length, batch_size, reset=0):
     result = []
     if reset == 0:
-        reset = predictionLength * 4
+        reset = prediction_length * 4
 
-    for i in range(int(len(data) / predictionLength)):
+    for i in range(int(len(data) / prediction_length)):
         # reset states
-        if (i * predictionLength) >= reset:
-            model.reset_states()
+        if (i * prediction_length) >= reset:
+            model.reset_states() # todo: research how this works in Session bookmarks folder
 
-        curr_frame = data[i * predictionLength]
+        curr_frame = data[i * prediction_length]
         predicted = []
-        # predict 1 window for current window, then shift by windowSize(==timesteps)
-        for j in range(predictionLength):
-            prediction = model.predict(curr_frame[np.newaxis, :, :], batch_size=batchSize)
+        # predict 1 window for current window, then shift by window_size(==timesteps)
+        for j in range(prediction_length):
+            prediction = model.predict(curr_frame[np.newaxis, :, :], batch_size=batch_size)
             predicted.append(prediction[0, 0])
 
             curr_frame = curr_frame[1:]
-            curr_frame = np.insert(curr_frame, [timesteps - 1], predicted[-1], axis=0)
+            curr_frame = np.insert(curr_frame, [timesteps - 1], prediction[0, 0], axis=0)
         result.append(predicted)
 
     result = np.array(result)
@@ -117,7 +114,7 @@ def predictMultiple(model, data, timesteps, predictionLength, batchSize, reset=0
     return result
 
 
-def predictSingle(model, data, timesteps, batch_size=1):
+def predict_single(model, data, timesteps, batch_size=1):
     # states are reset automatically after batch_size
     result = []
     print(data.shape)
@@ -132,97 +129,67 @@ def predictSingle(model, data, timesteps, batch_size=1):
     return result
 
 
-def calculateMAPE(prediction, y):
-    return np.mean(np.abs((y - prediction) / y)) * 100
-
-
-def plot_results(prediction, truth):
-    fig = plt.figure()
-
-    ax = fig.add_subplot(111)
-    ax.plot(truth, label='True Data')
-    plt.plot(prediction, label='Prediction')
-    plt.legend()
-    plt.show()
-
-
 if __name__ == '__main__':
-    np.random.seed(7)
-    train_ratio = 0.67
-    day = 96
-    week = day * 7
-    # testing parameters
+    ''' Testing parameters'''
+    train_ratio = 0.5
     epochs = 1
-    timesteps = 3
-    batchSize = 1
-    # true parameters
+    timesteps = 96
+    batch_size = 1
+    prediction_length = 96
+    ''' True parameteres'''
+    # train_ratio = 0.70
     # epochs = 5
-    # timesteps = day
-    # batchSize =
+    # timesteps = 96
+    # batch_size = 20
+    # prediction_length = 96
 
-    # dataset, trainX, trainY, testX, testY = load_data('smaller_sample.csv', timesteps, train_ratio)
-    dataset, trainX, trainY, testX, testY = load_data('bigger_sample.csv', timesteps, train_ratio)
-    # dataset, trainX, trainY, testX, testY = load_data('01_zilina_suma.csv', timesteps, train_ratio)
-    # model = createModel(trainX, trainY, epochs, batchSize)
-    print(testX)
-    print("**********")
-    print(testY)
+    # dataset, train_x, train_y, test_x, test_y = load_data('01_zilina_suma.csv', timesteps, train_ratio)
+    dataset, train_x, train_y, test_x, test_y = load_data('bigger_sample.csv', timesteps, train_ratio)
+    ''' Shape Check'''
+    # todo: make a check for train_x and prediction_length
+        # maybe doesnt have to exit(), but rather predict what it can and then just stop.
+    if dataset.shape[0] % batch_size is not 0:
+        print("Number of samples must be divisible by batch_size!")
+        exit()
+    print("train_x.shape == ", train_x.shape)
 
-    ''' SAVE & LOAD '''
-    # model.save('model.h5', True)
-    model = load_model('model.h5')
-    print("Model Loaded!")
-
-    # predict
-    model.reset_states()
-    prediction_single_keras = model.predict(testX, batch_size=batchSize)
-    model.reset_states()
-    prediction_multiple = predictMultiple(model, testX, timesteps, 3, batchSize)
-    model.reset_states()
-    prediction_single = predictSingle(model, testX, timesteps, batchSize)
-
-    # invert predictions
-    prediction_single_keras = scaler.inverse_transform(prediction_single_keras)
-    prediction_single = scaler.inverse_transform([prediction_single])
-    testY = scaler.inverse_transform([testY])
-    prediction_multiple = scaler.inverse_transform([prediction_multiple])
-    print("Predictions done!")
-    # print("TEST PREDICT:")
-    # print(testPredict)
-    # print("MULTIPLE PREDICTION:")
-    # print(testMultiple)
-    # print("testY:")
-    # print(testY)
-    # print("testPredict[:, 0]")
-    # print(testPredict[:, 0])
-
-    # print("testPredict len: ", len(testPredict))
-    # print("testMultiple[0] len: ", len(testMultiple[0]))
-    # print("testY[0] len: ", len(testY[0]))
-
-    print("single keras..", len(prediction_single_keras))
-    print(prediction_single_keras)
-    print("single custom..", len(prediction_single))
-    print(prediction_single)
-    print("multiple..", len(prediction_multiple))
-    print(prediction_multiple)
-    print("testY.. ", len(testY))
-    print(testY)
+    # model = createModel(train_x, train_y, epochs, batch_size)
+    print("dataset len: ", len(dataset))
+    print("train_x len: ", len(train_x))
+    print("train_y len: ", len(train_y))
+    print("test_x len: ", len(test_x))
+    print("test_y len: ", len(test_y))
     exit()
 
-    scoreSingle = math.sqrt(mean_squared_error(testY[0], prediction_single_keras[:, 0]))
-    print('scoreSingle: %.2f RMSE' % (scoreSingle))
-    scoreSingleMAPE = calculateMAPE(prediction_single_keras[:, 0], testY[0])
-    print("scoreSingleMAPE: %.2f MAPE" % (scoreSingleMAPE))
-    scoreSingleMAPE2 = calculateMAPE(prediction_single[:, 0], testY[0])
-    print("scoreSingleMAPE2: %.2f MAPE" % (scoreSingleMAPE2))
+    ''' Save & Load '''
+    # model.save('model.h5', True)
+    model = load_model('model_1_3_1_output1.h5')
+    print("Model Loaded!")
 
-    try:
-        scoreMultipleMAPE = calculateMAPE(np.append(prediction_multiple[0], testY[0][-1]), testY[0])
-        print("scoreMultipleMAPE: %.2f MAPE" % (scoreMultipleMAPE))
-        plot_results(np.append(prediction_multiple[0], testY[0][-1]), testY[0])
-    except ValueError as e:
-        print("didnt go through")
-        print(e)
+    ''' Predict '''
+    model.reset_states()
+    prediction_single_keras = model.predict(test_x, batch_size=batch_size)
+    model.reset_states()
+    prediction_multiple = predictMultiple(model, test_x, timesteps, prediction_length, batch_size)
+    model.reset_states()
+    prediction_single = predict_single(model, test_x, timesteps, batch_size)
+
+    ''' Invert Predictions to RL values'''
+    prediction_single_keras = scaler.inverse_transform(prediction_single_keras)
+    prediction_single = scaler.inverse_transform([prediction_single])
+    test_y = scaler.inverse_transform([test_y])
+    prediction_multiple = scaler.inverse_transform([prediction_multiple])
+    print("Predictions done!")
+
+    ''' Calculate MAPE and print'''
+    mape_single = calculate_mape(prediction_single_keras[:, 0], test_y[0])
+    mape_single2 = calculate_mape(prediction_single[:, 0], test_y[0])
+    mape_multiple = calculate_mape(prediction_multiple[0], test_y[0])
+    print("scoreSingleMAPE: %.2f MAPE" % (mape_single))
+    print("scoreSingleMAPE2: %.2f MAPE" % (mape_single2))
+    print("scoreMultipleMAPE: %.2f MAPE" % (mape_multiple))
+
+    ''' Plot Results'''
+    plot_results(prediction_multiple[0], test_y[0])
 
     gc.collect()
