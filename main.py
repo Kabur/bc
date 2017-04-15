8ba81f1be41da0f9c1b302c20bd5f49fb76939a6
Kabur <tomasfabry1@gmail.com>
2017-04-15 15:38:58 +0200

    added time of day to X data

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 import keras
-from keras.layers.core import Dense, Activation
+from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.models import load_model
@@ -46,13 +46,15 @@ def shuffle_in_unison(a, b):
 
 
 def load_data2(filename, timesteps, prediction_length, train_ratio, test_ratio, validation_ratio):
-    df = pd.read_csv(filename, usecols=[0, 2], engine='python')
+    df = pd.read_csv(filename, usecols=[0, 1, 2], engine='python')
 
-    # df['SUM_of_MNOZSTVO'] = df['SUM_of_MNOZSTVO'].values.astype('float32')
     df['DATUM'] = pd.to_datetime(df['DATUM'])
     df['day'] = df['DATUM'].dt.dayofweek
     df['day_sine'] = df['day'].apply(np.sin)
     df['day_cosine'] = df['day'].apply(np.cos)
+    df['time_sine'] = df['CAS'].apply(np.sin)
+    df['time_cosine'] = df['CAS'].apply(np.cos)
+    del df['CAS']
     del df['DATUM']
     del df['day']
     df['SUM_of_MNOZSTVO'] = scaler.fit_transform(df['SUM_of_MNOZSTVO'].values.reshape(-1, 1))
@@ -123,14 +125,18 @@ class LossHistory(keras.callbacks.Callback):
 
 def createModel(train_x, train_y, epochs, timesteps, batch_size, prediction_length, features=1):
     model = Sequential()
-    model.add(LSTM(48, batch_input_shape=(batch_size, timesteps, features), return_sequences=True, stateful=True))
-    model.add(LSTM(48, return_sequences=True, stateful=True))
+    model.add(LSTM(48, input_shape=(timesteps, features), return_sequences=True))
+    # model.add(Dropout(0.15))
+    model.add(LSTM(48, return_sequences=True))
+    # model.add(Dropout(0.15))
+    model.add(LSTM(48, return_sequences=True))
+    # model.add(Dropout(0.15))
     model.add(LSTM(48))
+    # model.add(Dropout(0.15))
     model.add(Dense(prediction_length))
     model.compile(loss='mean_squared_error', optimizer='adam')
     print(model.summary())
     # plot_model(model, to_file='model.png')
-
 
     history = LossHistory()
     model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=2, shuffle=True, callbacks=[history])
@@ -141,28 +147,28 @@ def createModel(train_x, train_y, epochs, timesteps, batch_size, prediction_leng
 
 
 if __name__ == '__main__':
-    features = 3
+    features = 5
     ''' Temp parameters'''
     # train_ratio = 0.50
     # test_ratio = 0.25
     # validation_ratio = 0.25
-    # epochs = 3
+    epochs = 1
     # timesteps = 6
     # batch_size = 10
     # prediction_length = 3
     ''' True Parameters '''
     train_ratio = 0.70
-    test_ratio = 0.20
-    validation_ratio = 0.10
+    test_ratio = 0.15
+    validation_ratio = 0.15
     epochs = 20
     timesteps = 96*4
     batch_size = 96*4
     prediction_length = 96
-    # todo: tweak parameters
 
     ''' Load Data '''
     dataset, train_x, train_y, test_x, test_y, validation_x, validation_y = load_data2('01_zilina_suma.csv', timesteps, prediction_length, train_ratio, test_ratio, validation_ratio)
     # dataset, train_x, train_y, test_x, test_y, validation_x, validation_y = load_data2('bigger_sample.csv', timesteps, prediction_length, train_ratio, test_ratio, validation_ratio)
+    # dataset, train_x, train_y, test_x, test_y, validation_x, validation_y = load_data2('smaller_sample.csv', timesteps, prediction_length, train_ratio, test_ratio, validation_ratio)
     print("Data Loaded!")
 
     ''' optional: Create Model'''
@@ -178,16 +184,19 @@ if __name__ == '__main__':
     # np.savetxt('loss_history.txt', np.array(history.losses), delimiter=',')
     ''' Save '''
     # model.save('model(10, 10, 10, 10, 96)_shape(384, 384, 3).h5', True)  # 9.43 MAPE after 20e
-    model.save('model(48, 48, 48, 48, 96)_shape(384, 384, 3)_20e.h5', True)
+    model.save('addedtime_model(48, 48, 48, 48, 96)_shape(384, 384, 3)_20e.h5', True)
     print("Model Saved!")
 
     ''' ********************************************************************** '''
     ''' Predict '''
     prediction = model.predict(test_x, batch_size=batch_size)
+    prediction2 = model.predict(validation_x, batch_size=batch_size)
 
     ''' Invert Predictions to RL values'''
     prediction = scaler.inverse_transform(prediction)
+    prediction = scaler.inverse_transform(prediction2)
     test_y = scaler.inverse_transform(test_y)
+    validation_y = scaler.inverse_transform(validation_y)
 
     ''' Calculate and print errors '''
     mape_per_vector = []
@@ -202,33 +211,29 @@ if __name__ == '__main__':
     print("prediction_vectors Median Error: %.2f" % median)
     print("prediction_vectors Standard Deviation of Error: %.2f" % standard_deviation)
 
+    mape_per_vector2 = []
+    for i in range(len(prediction2)):
+        mape_per_vector2.append(calculate_mape(prediction2[i], validation_y[i]))
+    mape_per_vector2 = np.array(mape_per_vector2)
+
+    mape2 = calculate_mape(prediction2, validation_y)
+    median2 = np.median(mape_per_vector2)
+    standard_deviation2 = np.std(mape_per_vector2)
+    print("prediction_vectors MAPE2: %.2f" % mape2)
+    print("prediction_vectors Median Error2: %.2f" % median2)
+    print("prediction_vectors Standard Deviation of Error2: %.2f" % standard_deviation2)
+
     ''' Plot Results'''  # saving fig to file doesnt work
     prediction_array = []
     y_array = []
     i = 0
-    while i < len(prediction):
-        prediction_array.append(prediction[i])
-        y_array.append(test_y[i])
+    while i < len(prediction2):
+        prediction_array.append(prediction2[i])
+        y_array.append(validation_y[i])
         i += prediction_length
 
     prediction_array = np.array(prediction_array).flatten()
     y_array = np.array(y_array).flatten()
     plot_results(prediction_array, y_array)
-
-    ''' SAVE RESULTS AND THE MODEL SUMMARY TO FILES '''
-    # orig_stdout = sys.stdout
-    # file = open('model_summary.txt', 'w')
-    # sys.stdout = file
-    # print(model.summary())
-    # print('epochs: ', epochs)
-    # print('timesteps: ', timesteps)
-    # print('batch_size: ', batch_size)
-    # print('prediction_length: ', prediction_length)
-    # print('features: ', features)
-    # print('mape: ', mape)
-    # print('median: ', median)
-    # print('standard_deviation: ', standard_deviation)
-    # sys.stdout = orig_stdout
-    # file.close()
 
     gc.collect()
